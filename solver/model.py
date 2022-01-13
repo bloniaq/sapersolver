17a6f747389a25ff50fafff2e80afe541691cf95
bloniaq <j.a.blonski@outlook.com>
2022-01-13 23:32:20 +0100

    Field class init

--- solver/model.py
+++ solver/model.py
@@ -5,10 +5,19 @@ logg = logging.getLogger('solver.c.model')
 
 
 class Board:
-
     COLUMNS = 30
     ROWS = 16
 
     def __init__(self, top, left) -> None:
         self.columns = self.COLUMNS
         self.rows = self.ROWS
+
+
+class Field:
+
+    def __init__(self, col: int, row: int, x: int, y: int) -> None:
+        self.col = col
+        self.row = row
+        self.x = x
+        self.y = y
+        self.state = 'covered'
--- tests/test_model.py
+++ tests/test_model.py
@@ -21,3 +21,32 @@ class Test_Model:
     def test_dimensions(self, basic_model):
         assert basic_model.columns == 30
         assert basic_model.rows == 16
+
+
+class Test_Field:
+
+    @pytest.fixture
+    def basic_field(self) -> Field:
+        COLUMN = 4
+        ROW = 8
+        X = 50
+        Y = 70
+        field = Field(
+            COLUMN,
+            ROW,
+            X,
+            Y
+        )
+        return field
+
+    def test_field_init(self, basic_field):
+        assert basic_field is not None
+        assert isinstance(basic_field, Field)
+        assert basic_field.col is not None
+        assert basic_field.row is not None
+        assert basic_field.x is not None
+        assert basic_field.y is not None
+        assert basic_field.state == 'covered'
+
+    # def test_neighbours(self, basic_field):
+        # assert
