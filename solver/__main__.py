3b70c464a02a32e1a9e253f9f5ddb60cf8d614b5
bloniaq <j.a.blonski@outlook.com>
2022-01-16 15:26:40 +0100

    Log file defined, formatter changed

--- solver/__main__.py
+++ solver/__main__.py
@@ -12,10 +12,16 @@ logg = logging.getLogger('solver')
 logg.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
+fh = logging.FileHandler('log.log', mode='w')
+fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    '%(asctime)s - %(levelname)s - %(message)s (- %(funcName)s )',
+    "%H:%M:%S"
+)
 ch.setFormatter(formatter)
+fh.setFormatter(formatter)
 logg.addHandler(ch)
+logg.addHandler(fh)
 
 if __name__ == '__main__':
 
