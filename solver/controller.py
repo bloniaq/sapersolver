81b62baea44a512e2e6ccd575fba30e319adde1b
bloniaq <j.a.blonski@outlook.com>
2022-01-16 15:29:35 +0100

    Solve algorithm simplified

--- solver/controller.py
+++ solver/controller.py
@@ -36,18 +36,24 @@ class Controller:
             logg.info("Started solving loop")
             fields_to_uncover = set()
 
-            potential_mines, neighbours_of_mines \
-                = self.model.get_potential_mines()
-            self.reader.mark_multiple_mines(potential_mines)
+            potential_mines, potential_numbers, neighbours_of_mines \
+                = self.model.get_potential()
+            self.reader.mark_multiple_field_as_mines(potential_mines)
             fields_to_uncover |= neighbours_of_mines
+            logg.debug(f"Neighbours of mines: {neighbours_of_mines}")
+            logg.debug(f"Potential numbers: {potential_numbers}")
+            fields_to_uncover |= potential_numbers
             fields_to_uncover |= self.model.get_complete_fields_w_cov_neighbours()
-            self.reader.uncover_multiple_fields(fields_to_uncover)
+            self.reader.uncover_around_multiple_fields(fields_to_uncover)
 
             logg.info("Going to update Board")
             changes = self.reader.update_board(fields_to_uncover)
             if not changes and not self.model.get_complete_fields_w_cov_neighbours():
                 i_know_what_to_do = False
             self.model.print_board()
+            # if self.reader.do_we_lost():
+            #     i_know_what_to_do = False
+            #     logg.error("WE LOST!")
         logg.warning("I don't know what to do")
 
     def _start_game(self):
@@ -59,7 +65,7 @@ class Controller:
                     return
 
         starting_field = self._pick_start_field()
-        self.reader.discover_field(starting_field)
+        self.reader.uncover_field(starting_field)
         self.reader.update_board({starting_field})
         logg.debug(f"Started new game at {starting_field}")
 
