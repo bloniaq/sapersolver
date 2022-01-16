7569149921fd5a5862fc8a18269b391929eaf2ed
bloniaq <j.a.blonski@outlook.com>
2022-01-16 23:20:34 +0100

    logging format edit

--- solver/__main__.py
+++ solver/__main__.py
@@ -15,7 +15,7 @@ ch.setLevel(logging.DEBUG)
 fh = logging.FileHandler('log.log', mode='w')
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(message)s (- %(funcName)s )',
+    '%(asctime)s - %(levelname)s - %(message)s (%(funcName)s)',
     "%H:%M:%S"
 )
 ch.setFormatter(formatter)
